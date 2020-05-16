dfadc7b80806bc7ee7926aae907481bcf4ca59ba
Soumya Roy <soumyar27@yahoo.com>
2020-05-16 18:08:20 -0400

    Update header for the function raw_data

--- bikeshare.py
+++ bikeshare.py
@@ -244,7 +244,7 @@ def user_stats(df):
 def raw_data(df):
     """
     Asks user if they want to see 5 lines of raw data.
-    Returns the 5 lines of raw data if user inputs `yes`. Iterate until user response with a `no`
+    Returns the 5 lines of raw data if user inputs `yes`. Iterate until user responds with a `no`.
 
     """
     # set a counter variable
