5b003b1f27f39c5cdb19f08ef74dbbf820005cab
Soumya Roy <soumyar27@yahoo.com>
2020-05-16 18:02:07 -0400

    Update header for time_stats function

--- bikeshare.py
+++ bikeshare.py
@@ -123,7 +123,7 @@ def load_data(city, month, day):
     return df
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most common times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
